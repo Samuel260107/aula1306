19ab420e4558fe08f0dd9ea694b2e0d73f28bcb2
Samuel260107 <samuel.americon02@gmail.com>
2024-06-14 08:32:44 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,66 @@
+# Calculadora simples
+
+while True:
+
+    print('\n\t\t\t -- Calculadora -- ')
+
+    print('1. soma')
+    print('2. subtração')
+    print('3. multiplicação')
+    print('4. divisão')
+    print('5. resto da divisão')
+    print('6. sair')
+    
+
+    op = int(input('\n\tOpção: '))
+
+    if op == 1:
+        print('\n\t\t\t -- Soma --\n')
+        
+        num1 = int(input('\n\tNum1: '))
+        num2 = int(input('\n\tNum2: '))
+        
+        resultado = num1 + num2 
+        
+        print('\n\tResultado: {}'.format(resultado))
+    elif op == 2:
+        print('\n\t\t\t -- Subtração --\n')
+        
+        num1 = int(input('\n\tNum1: '))
+        num2 = int(input('\n\tNum2: '))
+        
+        resultado = num1 - num2
+        
+        print('\n\tResultado: {}' .format(resultado))
+    elif op == 3:
+        print('\n\t\t\t -- Multiplicação --\n')
+        
+        num1 = int(input('\n\tNum1: '))
+        num2 = int(input('\n\tNum2: '))
+        
+        resultado = num1 * num2
+        
+        print('\n\tResultado: {}' .format(resultado))
+    elif op == 4:
+        print('\n\t\t\t -- Divisão --\n')
+        
+        num1 = int(input('\n\tNum1: '))
+        num2 = int(input('\n\tNum2: '))
+        
+        resultado = num1 / num2
+        
+        print('\n\tResultado: {}' .format(resultado))
+    elif op == 5:
+        print('\n\t\t\t -- Resto da Divisão --\n')
+        
+        num1 = int(input('\n\tNum1: '))
+        num2 = int(input('\n\tNum2: '))
+        
+        resultado = num1 % num2
+        
+        print('\n\tResultado: {}' .format(resultado))
+    elif op == 6:
+        print('\n Forte abraço!\n')
+        break
+    else:
+        print('Opção {} incorreta!'.format(op))
